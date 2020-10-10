7bb1a21d7392a34607e3da2f3cc34298ce94a37a
JeanCDev <jeangames15@gmail.com>
2020-10-10 18:33:46 -0300

    CRUD for reservations completed, users started

--- inc/users.js
+++ inc/users.js
@@ -50,6 +50,91 @@ module.exports = {
 
     });
 
+  },
+    
+// pega os usuários do banco de dados
+ getUsers(){
+  return new Promise((resolve, reject) => {
+
+      db.query(`
+        SELECT * FROM tb_users ORDER BY name
+        `, (err, results)=>{
+
+            if (err) { reject(err);} 
+            
+            resolve(results);
+
+        });
+
+      });
+
+    },
+
+  // salvar uma nova linha no banco de dados
+  save(fields, files) {
+
+    return new Promise((resolve, reject) =>{
+
+      let query, queryPhoto = '', params;
+
+      params = [
+        fields.name,
+        fields.email,
+      ];
+
+      if(parseInt(fields.id) > 0){
+
+        params.push(fields.id);
+
+        query = `
+          UPDATE tb_users
+          SET name = ?,
+              email = ?
+          WHERE id = ?
+        `;
+
+      } else {
+
+        query = `
+          INSERT INTO tb_menus (name, email, password)
+          VALUES(?, ?, ?)
+        `;
+
+        params.push(fields.password)
+
+      }
+
+      db.query(query, params, (err, results)=>{
+
+        if(err){ 
+          reject(err);
+        } else {
+          resolve(results);
+        }
+
+      });
+
+    });
+
+  },
+
+  // excluir uma linha do banco de dados 
+  delete(id){
+
+    return new Promise((resolve, reject) => {
+
+      db.query(`DELETE FROM tb_users WHERE id = ?`, [id], (err, results) => {
+
+        if(err){
+          reject(err);
+        } else {
+          resolve(results);
+        }
+
+      });
+
+    });
+
   }
 
 }
